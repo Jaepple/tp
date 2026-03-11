c5360ee1be785b7db2bb383e1361c5b60607a07c
Eunice Gwee <eunicegweee@gmail.com>
2026-03-11 22:41:41 +0800

    Fix Checkstyle violations

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -9,7 +9,6 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -43,7 +42,8 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_add_legacyAliasStillParses() throws Exception {
         Person person = new PersonBuilder().build();
-        AddCommand command = (AddCommand) parser.parseCommand(AddCommand.ALIAS + " " + PersonUtil.getPersonDetails(person));
+        String addCommand = AddCommand.ALIAS + " " + PersonUtil.getPersonDetails(person);
+        AddCommand command = (AddCommand) parser.parseCommand(addCommand);
         assertEquals(new AddCommand(person), command);
     }
 
@@ -79,7 +79,7 @@ public class AddressBookParserTest {
     public void parseCommand_find() throws Exception {
         List<String> keywords = Arrays.asList("foo", "bar", "baz");
         FindCommand command = (FindCommand) parser.parseCommand(
-                FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
+                FindCommand.COMMAND_WORD + " " + String.join(" ", keywords));
         assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
     }
 
